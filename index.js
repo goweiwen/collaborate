56f2f7179770f783a50107d3c32e16e44a7cf285
goweiwen <goweiwen@gmail.com>
2017-06-11 00:11:51 +0800

    Add passport Google OAuth2 authentication

--- index.js
+++ index.js
@@ -1,136 +1,127 @@
 import Koa from 'koa';
 import Router from 'koa-router';
-import multer from 'koa-multer';
-import serve from 'koa-static-server';
-import cors from 'kcors';
-import socketio from 'socket.io';
-import { applyMiddleware, createStore } from 'redux';
-import logger from 'redux-logger';
-import webpackMiddleware from 'koa-webpack';
-import { addTile, removeTile, addChatMessage, updateTile, updateLayout } from './actions';
-import reducer from './reducers/server';
 
-const PORT = 3000;
 const app = new Koa();
 const router = new Router();
 
 // Upload endpoint
+import multer from 'koa-multer';
 const upload = multer({ dest: 'public/uploads/' });
+router.post('/upload', upload.single('file'), async (ctx, next) => {
+  const { filename, originalname } = ctx.req.file;
+  ctx.body = { filename, originalname };
+  return await next();
+});
 
-// Routes
-router
-  .post('/upload', upload.single('file'), async (ctx, next) => {
-    const { filename, originalname } = ctx.req.file;
-    ctx.body = { filename, originalname };
-    return await next();
-  })
-  .get('/*', serve({ rootDir: 'public' }));
+// Logger
+import logger from 'koa-logger';
+app.use(logger());
 
-app
-  .use(cors())
-  .use(webpackMiddleware({ dev: {
-    'publicPath': '/assets/',
-    'lazy': false
-  } }))
-  .use(router.routes());
+// Sessions
+import session from 'koa-session';
+app.keys = [ 'wNxB5QD5W_x5CfbhNjvaVMJ-' ];
+app.use(session({}, app));
 
-     // Start server
-const server = app.listen(PORT, (err) => { if (err) console.log(err); });
-console.log('Listening on port 3000');
+// Body Parser
+import bodyParser from 'koa-bodyparser';
+app.use(bodyParser());
+
+// Authentication
+import passport from 'koa-passport';
+app.use(passport.initialize());
+app.use(passport.session());
+
+import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
+const GOOGLE_CLIENT_ID = '988996499797-tuq14tf6r4gh6hsghjop99o3is6nlt13.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = 'wNxB5QD5W_x5CfbhNjvaVMJ-';
+
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: '/login/callback',
+}, (accessToken, refreshToken, profile, done) => {
+  return done(null, profile.email);
+}));
+
+router.get('/login', passport.authenticate('google', {
+  scope: [ 'email' ]
+}));
+
+router.get('/login/callback', passport.authenticate('google', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
+// CORS
+import cors from 'kcors';
+app.use(cors());
+
+// Webpack
+import webpackMiddleware from 'koa-webpack';
+app.use(webpackMiddleware({ dev: {
+  'publicPath': '/assets/',
+  'lazy': false
+} }));
+
+// Static server
+import serve from 'koa-static-server';
+const staticServer = serve({ rootDir: 'public' });
+
+// Router
+router.get('/*', async (ctx) => {
+  if (ctx.isAuthenticated()) {
+    return await staticServer(ctx);
+  } else {
+    ctx.redirect('/login');
+  }
+});
+app.use(router.routes());
+
+// Start server
+const port = process.env.PORT || 3000;
+const server = app.listen(port, (err) => { if (err) console.log(err); });
+console.log(`Listening on port ${port}`);
 
 // Redux
 let state = {
-  
   tiles: [
-    {id: 0, tileType: 'text', content: '0',}, 
-    {id: 1, tileType: 'text', content: '1',}, 
-    {id: 2, tileType: 'text', content: '2',},
-    {id: 3, tileType: 'youtube', src: 'HtSuA80QTyo',},
-    {id: 4, tileType: 'googledoc', src: 'https://docs.google.com/document/d/1Xf0bxn-cvB18ycAxP27bDqeYAYq_JqKY6psZoPJuT-E/edit?usp=sharing',}  
+    { id: 0, tileType: 'text', content: '0' },
+    { id: 1, tileType: 'text', content: '1' },
+    { id: 2, tileType: 'pdf', page: 0,
+      src: 'uploads/The C Programming Language - 2nd Edition - Kernighan & Ritchie.pdf' },
+    { id: 3, tileType: 'youtube', src: 'HtSuA80QTyo' },
+    { id: 4, tileType: 'googledoc',
+      src: 'https://docs.google.com/document/d/1Xf0bxn-cvB18ycAxP27bDqeYAYq_JqKY6psZoPJuT-E/edit?usp=sharing' }
   ],
-
   layouts: {
-    0: {x:0, y:0, width:300, height:300},
-    1: {x:300, y:0, width:300, height:300},
-    2: {x:600, y:0, width:300, height:300},
-    3: {x:0, y:300, width:300, height:300},
-    4: {x:300, y:300, width:300, height:300},
-  },
+    0: { x:0, y:0, width:300, height:300 },
+    1: { x:300, y:0, width:300, height:300 },
+    2: { x:600, y:0, width:300, height:300 },
+    3: { x:0, y:300, width:300, height:300 },
+    4: { x:300, y:300, width:300, height:300 }
+  }
 };
 
-
-/* 
-
-messages: [
-    {id:0, user:'Admin', text:'Welcome'},
-    {id:1, user:'Nicholas', text:'World'}
-=======
-  messages: [
-    { id: 0, user: 'Admin', text: 'Welcome' },
-    { id: 1, user: 'Nicholas', text: 'World' }
->>>>>>> 90438733e00ce8a18b9be7a1d8026b85366c10f2
-  ],
-
-
-tiles: [
-    {
-      id: 0, tileType: 'youtube', src: 'HtSuA80QTyo',
-      layout: { x: 0, y: 0, width: 300, height: 300 }
-    },
-    {
-<<<<<<< HEAD
-      id: 1, tileType: 'image', src: 'https://unsplash.it/200/300?image=1',
-      layout: {x:300, y:0, width:300, height:300}
-=======
-      id: 1, tileType: 'image', src: 'https://unsplash.it/200/300',
-      layout: { x: 310, y: 0, width: 300, height: 300 }
->>>>>>> 90438733e00ce8a18b9be7a1d8026b85366c10f2
-    },
-    {
-      id: 2, tileType: 'text', content: 'hi',
-<<<<<<< HEAD
-      layout: {x:600, y:0, width:300, height:300},
-    }, 
-
-    {
-      id: 3, tileType: 'pdf', page: 0,
-      src: 'http://www.comp.nus.edu.sg/~cs2100/lect/cs2100-1-intro.pdf',
-      layout: {x:0, y:300, width:300, height:300}
-=======
-      layout: { x: 0, y: 310, width: 300, height: 300 },
-    },
-    {
-      id: 3, tileType: 'pdf', page: 0,
-      src: 'uploads/The C Programming Language - 2nd Edition - Kernighan & Ritchie.pdf',
-      layout: { x: 310, y: 310, width: 300, height: 300 }
->>>>>>> 90438733e00ce8a18b9be7a1d8026b85366c10f2
-    },
-
-     {
-      id: 4, tileType: 'pdf', page: 0,
-      src: 'uploads/Tutorial1.pdf',
-      layout: {x:300, y:300, width:300, height:300}
-    },
-
-    {
-      id: 5, tileType: 'pdf', page: 0,
-      src: 'http://www.adobe.com/content/dam/Adobe/en/devnet/acrobat/pdfs/pdf_open_parameters.pdf',
-      layout: {x:600, y:300, width:300, height:300}
-    },
-  ]*/
+import reduxLogger from 'redux-logger';
+import { applyMiddleware, createStore } from 'redux';
+import { addTile, removeTile, addChatMessage, updateTile, updateLayout } from './actions';
+import reducer from './reducers/server';
 
 const store = createStore(
   reducer,
   state,
-  applyMiddleware(logger)
+  applyMiddleware(reduxLogger)
 );
 
 // Socket.io
+import socketio from 'socket.io';
 const io = socketio(server);
 
 io.on('connection', (socket) => {
   console.log('user connected');
-  //console.log(store.getState().layouts)
   socket.emit('initiliase layouts', store.getState().layouts);
   socket.emit('initialise tiles', store.getState().tiles);
   socket.emit('initialise chat', store.getState().messages);
@@ -148,7 +139,6 @@ io.on('connection', (socket) => {
     store.dispatch(removeTile(id));
     store.dispatch(updateLayout(undefined, id));
     socket.broadcast.emit('remove', id);
-
   });
 
   socket.on('update tile', (tile)=> {
@@ -160,7 +150,6 @@ io.on('connection', (socket) => {
     store.dispatch(updateLayout(layout, id));
     socket.broadcast.emit('update layout', layout, id);
   });
-
 
   socket.on('add chat message', (message) => {
     store.dispatch(addChatMessage(message));
